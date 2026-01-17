06ae4b5b539a94b9f700f20db891b5c8019101e2
Alina <alina.kobru@gmail.com>
2026-01-17 10:57:41 -0700

    Added json functionality from frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage.tsx'; // Assume these components are created
 import SignupPage from './SignupPage.tsx';
+import VerifyPage from './VerifyPage.tsx'
 
 
 const App = () => {
@@ -10,9 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      {/* <Route path="/contact" element={<Contact />} /> */}
-      
-      {/* Add a fallback route for "page not found" */}
+      <Route path="/forgot-password" element={<VerifyPage />} />
       <Route path="*" element={<h1>404: Page Not Found</h1>} />
     </Routes>
   );
