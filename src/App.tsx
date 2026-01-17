449c76f9554649e0567714a60c23a9c906783793
AbiyaR2 <abiya.raheel@ucalgary.ca>
2026-01-17 14:12:36 -0700

    Fixed website error

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-// src/App.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage.tsx'; // Assume these components are created
-import SignupPage from './SignupPage.tsx';
+import Home from "./Home"
 
-
-const App = () => {
-  return (
-    <Routes>
-      {/* Main UI */}
-      <Route path="/" element={<Home />} />
-
-      {/* Auth */}
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      {/* <Route path="/contact" element={<Contact />} /> */}
-      
-      {/* Add a fallback route for "page not found" */}
-      <Route path="*" element={<h1>404: Page Not Found</h1>} />
-    </Routes>
-  )
+export default function App() {
+  return <Home />
 }
